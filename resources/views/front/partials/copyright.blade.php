0f6767156b4d6e05cd6969db66b3ae3238b64cc1
ibrahim-2li <ibrahim.2li@hotmail.com>
2025-02-18 12:03:58 +0300

    front end integration

---
+++ resources/views/front/partials/copyright.blade.php
@@ -0,0 +1,12 @@
+<div class="copyright">
+    <div class="row">
+        <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+            All Right Reserved. Made with love wire | <?php echo date('Y'); ?>
+        </div>
+        <div class="col-md-6 text-center text-md-end">
+            <div class="footer-menu">
+
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,37 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
+
+Route::prefix('/')->name('front.')->group(function (){
+    // ========================================== index
+    Route::view('', 'front.index')->name('index');
+    // ========================================== about
+    Route::view('about', 'front.about')->name('about');
+    // ========================================== contact
+    Route::view('contact', 'front.contact')->name('contact');
+    // ========================================== projects
+    Route::view('projects', 'front.projects')->name('projects');
+    // ========================================== services
+    Route::view('services', 'front.services')->name('services');
+    // ========================================== team
+    Route::view('team', 'front.team')->name('team');
+    // ========================================== testimonials
+    Route::view('testimonials', 'front.testimonials')->name('testimonials');
 });
